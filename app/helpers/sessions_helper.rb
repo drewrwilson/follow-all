5eae6a8d627cd71eef1260dff035ac9f7023338f
Drew <drew.r.wilson@gmail.com>
2014-05-28 10:34:41 -0400

    add some text and images about Transparency Camp

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,8 @@
 module SessionsHelper
-  FIRST_YEAR = 13
 
   def lists
     list_owner = 'tcampdc'
     lists = []
-    lists << 'attendees'
     lists << "TCamp14-attendees"
     lists << "TCamp13-attendees"
     lists.collect do |list|
@@ -12,7 +10,3 @@ module SessionsHelper
     end
   end
 end
-
-
- # lists << "TCamp14 Attendees"
- #    lists << "TCamp13 Attendees"
